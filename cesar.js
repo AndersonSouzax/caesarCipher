7d036eb04785e0fc70251e4b18fbe5e55fb1c42a
Anderson <anderson.souzax@hotmail.com>
2018-05-21 00:22:02 -0300

    quase feito

--- cesar.js
+++ cesar.js
@@ -1,20 +1,24 @@
 //ALGORITMO CIFRA DE CÉSAR
 
-//Criptografar
-/* No pior caso, a rotação será igual o tamanho da mensagem */
+//Criptografar ou Descriptografar
+/* No pior caso, a rotação será igual o número de letras no alfabeto - 1 */
 /* @direction: direção da rotação: 0 = esquerda, 1 = direita */
 /* Assumindo o alfabeto maiúsculo brasileiro */
-function encrypt(direction, rotation, message){
+function encrypt(direction, rotation, message, decrypt){
 
+    //Tratamentos simples
     if( rotation > 25 ){ rotation = 25; }
     if(!message || message.trim() == ''){
         return '';
     }
 
+    //Ao descriptografar muda-se apenas a direção
+    if(decrypt){ direction = !direction; }
+
     //Transformando em Array
     var messageArray = message.toUpperCase().split('');
 
-    var cipheredMessage = '';
+    var cipheredOrNormalMessage = '';
     var alphabet = ['A','B','C','D','E','F','G','H','I','J',
                     'K','L','M','N','O','P','Q','R','S','T','U',
                     'V','W','X','Y','Z'];
@@ -23,30 +27,46 @@ function encrypt(direction, rotation, message){
     //Iterando a mensagem
     for(i; i < size;i++){
         
-        if(!messageArray[i] != ' '){
+        if(messageArray[i] != ' '){
 
             //Iterando o alfabeto
             for(j=0;j < alphabet.length;j++){
 
                 if(alphabet[j] == messageArray[i]){
                     
+                    //ROTAÇÃO À ESQUERDA
                     if(!direction){
-                        dif = j - rotation;
-                        index = dif - 25 < 0 ? 26 - Math.abs(dif) : dif;
+                        dif =  j - rotation;
+                        index = dif < 0 ? 26 - Math.abs(dif) : dif;
+                    
+                    //ROTAÇÃO À DIREITA
                     }else{
-                        dif = j + rotation;
-                        index = dif + 25 > 25 ? dif - 26 : dif;
-                    }
 
-                    cipheredMessage += alphabet[index];
+                        dif = j + rotation;
+                        index = dif > 25 ? dif - 26 : dif;
+                        
+                    }
+                    cipheredOrNormalMessage += alphabet[index];
                     break;
                 }
             }
 
-        }
-
-        cipheredMessage += ' ';
+        }else{
+            cipheredOrNormalMessage += ' ';
+        }     
     }
+    //Imprimindo mensagem
+    document.getElementById('enc-box').innerHTML = cipheredOrNormalMessage;
 
-    return cipheredMessage;
+    return cipheredOrNormalMessage;
+}
+
+function getInfoAndDec(){
+
+    var message = document.getElementById('box').value;
+    var direction = document.getElementById('select-direction').selectedIndex;
+    var cripto = document.getElementById('select-cripto').selectedIndex;
+    var rotation = parseInt(document.getElementById('rotation').value);
+debugger;
+    encrypt(direction,rotation,message,cripto);
 }
